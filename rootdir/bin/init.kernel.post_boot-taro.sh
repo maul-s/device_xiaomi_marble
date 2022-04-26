095cebf245e4f6fa88e1113de93f65f88c0eb86c
Vishalcj17 <vishalcj@aospa.co>
2022-04-25 20:56:35 -0400

    marble: rootdir: Adjust foreground cpuset
    * Other SoCs are welcome here. Please test thoroughly before, especially power usage.
    * Power usage same as before and noticeable decrease in jitter on taro.
    
    Change-Id: I899ece7b48d7958756c16ce6393995f6ac86d5d6
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- rootdir/bin/init.kernel.post_boot-taro.sh
+++ rootdir/bin/init.kernel.post_boot-taro.sh
@@ -81,11 +81,11 @@ echo 15 15 15 15 15 15 15 15 > /proc/sys/walt/sched_util_busy_hyst_cpu_util
 echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 
 # cpuset parameters
-echo 0-1 > /dev/cpuset/background/cpus
-echo 0-1 > /dev/cpuset/restricted/cpus
-echo 0-2 > /dev/cpuset/system-background/cpus
-echo 0-3 > /dev/cpuset/foreground/cpus
-echo 0-4,7 > /dev/cpuset/top-app/cpus
+echo 0-2 > /dev/cpuset/background/cpus
+echo 0-6 > /dev/cpuset/foreground/cpus
+echo 0-3 > /dev/cpuset/system-background/cpus
+echo 4-7 > /dev/cpuset/foreground/boost/cpus
+echo 0-7 > /dev/cpuset/top-app/cpus
 
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/walt/sched_boost
