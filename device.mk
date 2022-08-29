3148d4ca38a4126bf2c10d242905e84f4f5a7829
Terminator-J <terminator_j@hotbutteredmonkey.com>
2022-08-29 10:19:11 -0700

    sdm845-common: thermal: Import qcom-caf thermal HAL *From https://github.com/LineageOS/android_hardware_qcom_thermal/ lineage-21.0 branch.

--- device.mk
+++ device.mk
@@ -671,7 +671,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0.vendor
+    android.hardware.thermal@2.0-service.sdm845
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
