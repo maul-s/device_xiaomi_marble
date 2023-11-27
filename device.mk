fbf35259393906222ce14fb51d8b9800457d9612
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-11-27 06:31:26 +0530

    fixup! marble: Move display (SDM) stack to garnet
    Apparently this is still required.

--- device.mk
+++ device.mk
@@ -233,6 +233,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.sf.defer_refresh_rate_when_off=1 \
+    ro.sf.force_handle_idle_timeout=1
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
