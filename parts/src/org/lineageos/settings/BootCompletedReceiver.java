d01a6320d50ea50243b7cbbec90c20e1122e3182
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-12-05 15:34:08 +0530

    marble: parts: Relay display orientation to touchpanel
    Required by our touchscreen to determine bottom position for
    edge rejection.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -26,6 +26,7 @@ import org.lineageos.settings.display.ColorService;
 import org.lineageos.settings.camera.NfcCameraService;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
+import org.lineageos.settings.touch.TouchOrientationService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -66,5 +67,8 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         displayManager.overrideHdrTypes(Display.DEFAULT_DISPLAY, new int[]{
                 HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
                 HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
+        
+        // Touch Orientation Service
+        TouchOrientationService.startService(context);
     }
 }
