508aae02737be81c23e3c3353bd2039f3e7c2e3a
Maulana Riaddy <maulriaddy@gmail.com>
2024-01-11 11:07:04 +0700

    marble: Mist os marble a14

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_marble.mk
+    $(LOCAL_DIR)/mist_marble.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_marble-eng \
-    aosp_marble-userdebug \
-    aosp_marble-user
+    mist_marble-eng \
+    mist_marble-userdebug \
+    mist_marble-user
--- mist_marble.mk
+++ mist_marble.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Mist OS stuff.
+$(call inherit-product, vendor/mist/config/common_full_phone.mk)
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
@@ -26,7 +26,14 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := true
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_marble
+PRODUCT_NAME := mist_marble
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_GAPPS_ARCH := arm64
+
+# Matrixx
+MATRIXX_MAINTAINER := SMGRebornXMauls
+MATRIXX_CHIPSET := SM7475
+MATRIXX_BATTERY := 5000mah
+MATRIXX_DISPLAY := 1080x2400
