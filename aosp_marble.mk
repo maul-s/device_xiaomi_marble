71cc33390a5b227a9be7eb0a61216e60b9b69eb7
Maulana Riaddy <maulriaddy@gmail.com>
2024-01-11 14:15:05 +0700

    marble: Bringup firedroid

--- aosp_marble.mk
+++ aosp_marble.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common Firedroid stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from marble device.
@@ -30,3 +30,10 @@ PRODUCT_NAME := aosp_marble
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Gapps
+WITH_GAPPS := true
+
+# FireDroid Maintainer Flags
+FIREDROID_MAINTAINER := Maul's
+CUSTOM_BUILD_TYPE := OFFICIAL
