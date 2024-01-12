7a2e76577828827faf929041b3dc806265b97ee9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-12 17:41:21 +0530

    marble: Add support for high touch polling rate

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -26,6 +26,7 @@ import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.doze.PocketService;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.touch.HighTouchPollingService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -48,6 +49,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // NFC
         NfcCameraService.startService(context);
+        HighTouchPollingService.startService(context);
 
         // Pocket
         PocketService.startService(context);
