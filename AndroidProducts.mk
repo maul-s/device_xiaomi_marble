4b75ac0bb78733ff718396d2bdfa0527763ac5ba
mauls-xyz <maulriaddy23@gmail.com>
2024-01-15 03:25:24 +0700

    marble: Bringup apollo os

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_marble.mk
+    $(LOCAL_DIR)/apollo_marble.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_marble-eng \
-    lineage_marble-userdebug \
-    lineage_marble-user
+    apollo_marble-eng \
+    apollo_marble-userdebug \
+    apollo_marble-user
--- apollo_marble.mk
+++ apollo_marble.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Apollo stuff.
+$(call inherit-product, vendor/apollo/config/common_full_phone.mk)
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
@@ -18,7 +18,11 @@ $(call inherit-product, device/xiaomi/marble/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_marble
+PRODUCT_NAME := apollo_marble
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_GAPPS_ARCH := arm64
+
+# Gapps
+WITH_GAPPS := true
